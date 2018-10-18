309479df060049e7f25fe508becacdbebc45cfc7
mohamed-hsan <43474223+mohamed-hsan@users.noreply.github.com>
2018-10-18 15:26:56 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,5 +27,30 @@ client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
 });
+client.on("message", message => {
+	var prefix = "-";
+ if (message.content === "-help") {
+  const embed = new Discord.RichEmbed()  
+      .setColor("#000000") 
+      .setDescription(`
+	  
+	         Please Chose: 
 
+			 
+                               ►الاوامر العامة ◄
+$server - معلومات عن سيرفرك
+$id - معلومات عنك
+$avatar [user] - لعرض صورتك او صوره شخص
+$image - لعرض صورة السيرفر 
+$owner - لإرسال رسالة لاونر سيرفر 
+$user - معلومات عن حسابك
+$color   : لتغيير لونك في السيرفر
+$colors  : قائمة الألوان
+$credits  : التحويل ومعرفة الرصيد
+$short  : أختصار الروابط
+	  `)
+   message.channel.sendEmbed(embed)
+    
+   }
+   }); 
 client.login(process.env.BOT_TOKEN);
