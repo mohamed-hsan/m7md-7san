7bfb2023b6109e1a2d6a69f901a41634716b0464
mohamed-hsan <43474223+mohamed-hsan@users.noreply.github.com>
2018-10-19 15:08:29 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,26 +27,21 @@ client.user.setGame(`Noting`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
 });
-client.on('message', message => {
-    if (message.content.startsWith("$avatar")) {
-        if (message.author.bot) return
-        var mentionned = message.mentions.users.first();
-    var omar;
-      if(mentionned){
-          var omar = mentionned;
-      } else {
-          var omar = message.author;
-          
-      }
-        const embed = new Discord.RichEmbed()
-        .setColor("RANDOM")
-        .setAuthor('Avatar Link :')
-        .setTitle('Click Here')
-        .setURL(`${omar.avatarURL}`)
-        .setImage(`${omar.avatarURL}`)
-        .setFooter('iron bot',client.user.avatarURL) 
-      message.channel.sendEmbed(embed);
-    }
-});
+client.on("message", message => {
 
+          if(!message.channel.guild) return;
+   if(message.author.bot) return;
+      if(message.content === prefix + "$image"){ 
+          const embed = new Discord.RichEmbed()
+  
+      .setTitle(`This is  ** ${message.guild.name} **  Photo !`)
+  .setAuthor(message.author.username, message.guild.iconrURL)
+    .setColor(0x164fe3)
+    .setImage(message.guild.iconURL)
+    .setURL(message.guild.iconrURL)
+                    .setTimestamp()
+
+   message.channel.send({embed});
+      }
+  }); 
 client.login(process.env.BOT_TOKEN);
