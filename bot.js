c01470ae6072b47b4b36a62d39c4b95498920c08
mohamed-hsan <43474223+mohamed-hsan@users.noreply.github.com>
2018-10-19 14:19:08 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,49 +27,5 @@ client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
 });
-client.on('message', function(message) {
-  if (message.content.startsWith(prefix + 'info')) {
-  message.delete;
-  message.channel.send("", {
-      embed: {
-          color : 0xE15306,
-  author :{name: message.author.name},
-          description : "",
-          title : "Server Info",
-          fields : [
-              {
-   name : '**Server Name**',
-   value : message.guild.name,
-   inline : true
-            },{
-   name : '**Owner**',
-   value : message.guild.owner.user.tag,
-   inline : true
- },{
-    name :'**Verification Level**',
-    value : message.guild.verificationLevel,
-    inline : true
- },{
-    name : '**Region**',
-    value : message.guild.region,
-    inline : true
- },{
-   name : '**Members Count**',
-   value : message.guild.memberCount,
-   inline : true
- },{
-   name : '**Humans**',
-   value : message.guild.members.filter(m => m.user).size,
-   inline : true
- },{
-   name : '**Bots**',
-   value : message.guild.members.filter(m => m.user.bot).size,
-   inline : true
- }],
-},
-footer : {
-   text : 'RANDOM'
-          }
-    });
-}
+
 client.login(process.env.BOT_TOKEN);
