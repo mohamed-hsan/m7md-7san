9729b2ef62c56788d9f595b540e79a300ed3c39c
mohamed-hsan <43474223+mohamed-hsan@users.noreply.github.com>
2018-10-19 16:46:12 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,20 +28,22 @@ client.user.setGame(`Noting`,"http://twitch.tv/S-F")
   console.log('')
 });
 client.on("message", message => {
-var prefix = "$"
-          if(!message.channel.guild) return;
-   if(message.author.bot) return;
-      if(message.content === prefix + "image"){ 
-          const embed = new Discord.RichEmbed()
-  
-      .setTitle(`This is  ** ${message.guild.name} **  Photo !`)
-  .setAuthor(message.author.username, message.guild.iconrURL)
-    .setColor(0x164fe3)
-    .setImage(message.guild.iconURL)
-    .setURL(message.guild.iconrURL)
-                    .setTimestamp()
-
-   message.channel.send({embed});
-      }
-  }); 
+    let args = message.content.split(" ").slice(1);
+  if (message.content.startsWith(prefix + 'owner')) {
+        let user = message.mentions.users.first();
+        let reason = args.slice(1).join(' ');
+        let modlog = client.channels.find('name', 'call-owner');
+        if (!reason) return message.reply('**ضع سبباً مقنعاً**');
+   
+    if (!modlog) return message.reply('**لا يوجد روم بأسم call-owner**');
+    const embed = new Discord.RichEmbed()
+      .setColor(0x00AE86)
+      .setTimestamp()
+      .addField('نوع الرسالة:', 'call-owner')
+      .addField('المراد قوله :', reason);
+      message.delete()
+      return client.channels.get(modlog.id).sendEmbed(embed).catch(console.error);
+      console.log('[call owner] Send By: ' + message.author.username)
+  }
+  });
 client.login(process.env.BOT_TOKEN);
