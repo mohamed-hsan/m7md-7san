1787ee9b31b86c3a4c94bce7bddacf15b106d468
mohamed-hsan <43474223+mohamed-hsan@users.noreply.github.com>
2018-10-21 23:04:04 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,7 +33,7 @@ client.on('message', msg => {
   let command = msg.content.split(" ")[0];
   command = command.slice(prefix.length);
   let args = msg.content.split(" ").slice(1);
-
+ 
     if(command === "clear") {
         const emoji = client.emojis.find("name", "wastebasket")
     let textxt = args.slice(0).join("");
