ea405043dbfb00519b07ae72ffae502f2b31a732
mohamed-hsan <43474223+mohamed-hsan@users.noreply.github.com>
2018-10-21 20:00:03 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,5 +27,20 @@ client.user.setGame(`Noting`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
 });
+client.on('message', message => { 
+ let args = message.content.split(' ').slice(1);
+    if(message.content.startsWith(prefix + 'short')) {
+    if(!message.channel.guild) return;  
 
+        googl.setKey('AIzaSyC2Z2mZ_nZTcSvh3QvIyrmOIFP6Ra6co6w');
+        googl.getKey();
+        googl.shorten(args.join(' ')).then(shorturl => {
+            message.channel.send(''+shorturl)
+        }).catch(e=>{
+            console.log(e.message);
+            message.channel.send('Error!');
+            
+        });
+}
+});
 client.login(process.env.BOT_TOKEN);
