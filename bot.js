8dcf8d7eaeb65473ba8e155f2890113d4bb3773c
mohamed-hsan <43474223+mohamed-hsan@users.noreply.github.com>
2018-10-21 20:18:31 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -47,10 +47,10 @@ client.on('message', message => {
   /*let b5bzlog = client.channels.find("name", "5bz-log");
  
   if(!b5bzlog) return message.reply("I've detected that this server doesn't have a 5bz-log text channel.");*/
-  if (message.mentions.users.size < 1) return message.channel.send(`https://cdn.pg.sa/fjxlms81nk.png`);
+  if (message.mentions.users.size < 1) return message.channel.send(`لازم تحدد مدة الباند`);
   if(!reason) return message.channel.send(`https://cdn.pg.sa/fjxlms81nk.png`);
   if (!message.guild.member(user)
-  .bannable) return message.reply(`This User Is Have High Role !`);
+  .bannable) return message.reply(`لا يمكنني طرد شخص اعلى من رتبتي`);
  
   message.guild.member(user).ban(7, user);
  
