c8772afb1651396d2e3ea2a9222ed8ea3a85154d
mohamed-hsan <43474223+mohamed-hsan@users.noreply.github.com>
2018-10-23 22:04:50 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -461,4 +461,15 @@ Servers Counter : __${client.guilds.size}__**`)
 
 );
 
+client.on('guildMemberAdd', member => {
+    var embed = new Discord.RichEmbed()
+    .setThumbnail(member.user.avatarURL)
+  .addField("***شكرا الانضمامك الينا***" ,member.user.username )
+    .setDescription('***بكل حب واحترام وشوق نستقبلك ونتمنى لك قضآء أجمل اللحظات ولآوقات معنا***')
+    .setColor('RANDOM')
+    .setImage('http://www.imgion.com/images/01/Welcome-buddy.jpg')
+var channel =member.guild.channels.find('name', 'welcome-leave')
+if (!channel) return;
+channel.send({embed : embed});
+});
 client.login(process.env.BOT_TOKEN);
