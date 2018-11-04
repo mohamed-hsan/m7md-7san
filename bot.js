93ab7bb6f61a4c83e1d6c3bb765314d7d8861b47
mohamed-hsan <43474223+mohamed-hsan@users.noreply.github.com>
2018-11-04 14:05:33 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,70 +27,31 @@ client.user.setGame(``,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
 });
- client.on('message', message => {
-    if (message.author.bot) return;
-     if (message.content === prefix + "help") 
+ client.on("message", message => {
+    var prefix = "$";
+ if (message.content === "$help") {
+      .setDescription(►الاوامر العامة ◄
+$avatar [user] - لعرض صورتك او صوره شخص
+$image - لعرض صورة السيرفر 
+$owner - لإرسال رسالة لاونر سيرفر 
+$id - معلومات عنك
+$server  : لعرض معلومات عن السيرفر
+▁ ▂ ▃ ▄ ▅ ▆ ▇ █   █ ▇ ▆ ▅ ▄ ▃ ▂ ▁
+►الاوامر الادارية ◄
+$ban : لتبنيد شخص من السيرفر
+$kick - لطرد شخص من السيرفر
+$mute : اعطاء ميوت لشخص معين
+$unmute : لفك الميوت عن شخص معين
+$clear : لمسح الشات مع عدد او بدون
+$role  : لأعطاء رتبة لشخص معين
+▁ ▂ ▃ ▄ ▅ ▆ ▇ █   █ ▇ ▆ ▅ ▄ ▃ ▂ ▁
+►أشياء اخرى◄
+$invite  : لدعوة البوت الى سيرفرك
+$support  : سيرفر الدعم الفني
+▁ ▂ ▃ ▄ ▅ ▆ ▇ █   █ ▇ ▆ ▅ ▄ ▃ ▂ ▁
+)
+   message.channel.sendEmbed(embed)
 
-
-
- message.author.sendMessage(`
- **
-[❖═════ General Commands ═══════❖]
-
- #id معلومات عن حسابك الشخصي
-
- #server معلومات حول السيرفر
- 
- #move سحب عضو الى رومك الصوتي
-
- #clear مسح الرسائل الموجوده في الروم بعدد
-
- #avatar يعرض اك صورتك الشخصية
- 
- #image يعرض لك صورة السيرفر
- 
- #credit يوريك كم الكريديت حقتك
-
- #daily يسوي لك سحب فلوس
-
- #rep يعطي ريب
-
- #profile معلومات عامة مع الصورة
- 
-[❖═════ Administrator Commands ═══════❖]
-
- #ban حضر عضو من السيرفر
- 
- #kick طرد عضو من السيرفر
- 
- #mute اعضاء ميوت كتابي لعضو في السيرفر
- 
- #unmute فك الميوت عن عضو في السيرفر
- 
- #dac حذف جميع رومات السيرفر
- 
- #dar حذف جميع رتب السيرفر
- 
- #openroom فتح المحادثة في الروم
- 
- #closeroom قفل المحادثة في الرةوم
-
- #role اعطاء رتبه لشخض معين
- 
- #role humans اعطاء رتب للبشريين
- 
- #role bots اعطاء رتبه للبوتات
- 
- #role all اعطاء رتبه للجميع سواء بشر او بوتات
- 
-[❖═════ Other ═══════❖]
-
- #support رابط سيرفر الدعم الفني
- 
- #invite رابط اضافة البوت
-
- **`);
-
-    }
-});
+   }
+   });
 client.login(process.env.BOT_TOKEN);
